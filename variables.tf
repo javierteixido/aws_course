11e521369a5fe483b019119dca5f5aa972e98e37
= <=>
2026-02-12 15:36:08 +0100

    rest of files

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["eu-west-1a", "eu-west-1b"]
+}
